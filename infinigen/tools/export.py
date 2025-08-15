b998ea40be1acdff482c3051e4f90872feadf19b
liver121888 <liver121888@gmail.com>
2025-08-15 14:40:34 -0700

    bug fix from main stream

--- infinigen/tools/export.py
+++ infinigen/tools/export.py
@@ -143,7 +143,7 @@ def split_glass_mats():
         if split_obj in obj.name
     ]
     for match in matches:
-        if len(match.material_slots) == 0:
+        if len(match.material_slots) == 0 or match.material_slots[0] is None:
             continue  # skip objects without material slots
 
         mat = match.material_slots[0].material
